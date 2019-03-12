7d2733d658f790db0d5d117b64e6427fd795d225
Alexander Efimov <efimokthefatass@gmail.com>
2019-03-12 09:00:22 +0300

    Correct spelling mistakes

---
+++ docs/data-table/precache-manifest.1c1a37e663e02f43cfd8f1a65f1ae371.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf3f1983e7ef68c1db57",
+    "url": "/static/css/main.4f59c444.chunk.css"
+  },
+  {
+    "revision": "bf3f1983e7ef68c1db57",
+    "url": "/static/js/main.bf3f1983.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "73585d8f7245dcf4f406",
+    "url": "/static/js/2.73585d8f.chunk.js"
+  },
+  {
+    "revision": "83ff8a064faa5d28f95b29275ac9fb76",
+    "url": "/index.html"
+  }
+];
