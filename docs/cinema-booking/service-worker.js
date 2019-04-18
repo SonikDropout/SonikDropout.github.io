e742c14cbd0085acd1117a7484c37ea03046e959
Alexander Efimov <efimokthefatass@gmail.com>
2019-04-18 12:57:56 +0300

    polyfills for bell task

--- docs/cinema-booking/service-worker.js
+++ docs/cinema-booking/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/docs/cinema-booking/precache-manifest.650ccd3bd45719095b3c0bcec74d6acd.js"
+  "/precache-manifest.91f1c6fe0ba510586be5750096cada70.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/docs/cinema-booking/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
